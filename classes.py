8b2787cb2ba6495f62f45904d85f3afae4e9decd
Bryce Robinson <ak.bryce.robinson@gmail.com>
2020-06-03 13:45:00 -0600

    some functions

--- classes.py
+++ classes.py
@@ -1,6 +1,6 @@
 #--------------------------------------------------------------CLASSES---------------------------------------------------------------------------
 class Character:
-    def __init__(self, race=None, gender=None, name=None, class_name=None, sub_class=None, background=None, personality_traits=None, alignment=None, languages=None, stats=None, level=None):
+    def __init__(self, race=None, gender=None, name=None, class_name=None, sub_class=None, background=None, personality_traits=None, alignment=None, languages=None, stat_dict={"Str":0, "Dex":0, "Con":0, "Wis":0, "Int":0, "Cha":0}, level=None):
         self.race = race
         self.gender = gender
         self.name = name
@@ -10,29 +10,48 @@ class Character:
         self.personality_traits = personality_traits
         self.alignment = alignment
         self.languages = languages
-        self.stats = stats
+        self.stat_dict = stat_dict
         self.level = level
     
     def __repr__(self):
         returnable = ""
         return returnable.format()
 
-    def add_race():
-        pass
-    def add_gender():
-        pass
+    
+    def add_race(self, race):
+        self.race = race
+    
+    def add_gender(self, gender):
+        if gender is "M" or "m":
+            gender = "Male"
+        elif gender is "F" or "f":
+            gender = "Female"
+        self.gender = gender
+    
     def add_name():
         pass
+    
     def add_class():
         pass
+    
     def add_sub_class():
         pass
+    
     def add_background():
         pass
-    def add_alignment():
-        pass
-    def add_languages():
-        pass
+    
+    def add_alignment(self, word1, word2):
+        if word1 is "Lawful" or "Neutral" or "Chaotic":
+            if word2 is "Good" or "Neutral" or "Evil":
+                self.alignment= word1+" "+word2 
+    
+    def add_languages(self, languages_list):
+        for language in languages_list:
+            self.languages.append(language)
+    
     def set_stat(self, stat_type, stat_value):
-        if stat_type in "StrDexConWisIntCha":
-            
+        if stat_type is ("Str" or "Dex" or "Con" or "Wis" or "Int" or "Cha"):
+            if (stat_value > 0) and (stat_value <= 18):
+                self.stat_dict.update(stat_type= stat_value)
+
+        else: print("Error, invalid stat type detected.")
