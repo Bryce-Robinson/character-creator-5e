519e1f9c74c9320724606a2777980d0e62989b15
Bryce Robinson <ak.bryce.robinson@gmail.com>
2020-06-03 19:20:05 -0600

    made an outline

---
+++ statrollsys.py
@@ -0,0 +1,18 @@
+import random
+
+def three_d_six_strict():
+    pass 
+
+def three_d_six_three_times_use_highest():
+    pass
+
+def four_d_six_drop_lowest():
+    pass
+
+def four_d_six_drop_lowest_reroll_ones():
+    pass
+
+def point_buy(point_amount):
+    #the KEY is the STAT number, the VALUE is the POINTS it cost
+    point_buy_dict={4:-4, 5:-3, 6:-2, 7:-1, 8:0, 9:1, 10:2, 11:3, 12:4, 13:5, 14:7, 15:9}
+
