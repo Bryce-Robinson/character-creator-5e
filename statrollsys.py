b4881bcdfa38217b78c7c991c5787bdd1afdd539
Bryce Robinson <ak.bryce.robinson@gmail.com>
2020-06-03 19:54:48 -0600

    renamed tuple

--- statrollsys.py
+++ statrollsys.py
@@ -14,7 +14,7 @@ def four_d_six_drop_lowest_reroll_ones():
 
 def point_buy(point_amount=25):
     #the KEY is the STAT number, the VALUE is the POINTS it cost, max stat in PB is 15.
-    tuple_point_buy=(4,-4,5,-3,6,-2,7,-1,8,0,9,1,10,2,11,3,12,4,13,5,14,7,15,9)
+    point_buy_tuple=(4,-4,5,-3,6,-2,7,-1,8,0,9,1,10,2,11,3,12,4,13,5,14,7,15,9)
     count = 0
     print("Stat values:", end="\t")
     while count < len(tuple_point_buy)-1:
