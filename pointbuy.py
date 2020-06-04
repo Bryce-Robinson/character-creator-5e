d48fb46b38c1224d2a4de0a01ba90e1537b095d8
Bryce Robinson <ak.bryce.robinson@gmail.com>
2020-06-03 19:25:21 -0600

    renamed pointbuy -> statrollsys

--- pointbuy.py
+++
@@ -1,2 +0,0 @@
-def point_buy(point_amount):
-    pass
--- statrollsys.py
+++ statrollsys.py
@@ -12,7 +12,16 @@ def four_d_six_drop_lowest():
 def four_d_six_drop_lowest_reroll_ones():
     pass
 
-def point_buy(point_amount):
-    #the KEY is the STAT number, the VALUE is the POINTS it cost
+def point_buy(point_amount=25):
+    #the KEY is the STAT number, the VALUE is the POINTS it cost, max stat in PB is 15.
     point_buy_dict={4:-4, 5:-3, 6:-2, 7:-1, 8:0, 9:1, 10:2, 11:3, 12:4, 13:5, 14:7, 15:9}
+    
 
+
+
+
+
+
+
+
+point_buy(1)
